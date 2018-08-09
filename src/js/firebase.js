0231c1773fc4c7df6e838cca8cdb5aa7ae2bd200
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-08-09 15:20:24 -0500

    Agregando vista de administrador y listado de visitantes

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -6,39 +6,88 @@ window.initializeFirebase = () => {
     projectId: "proyecto-5-coworking-developer",
     storageBucket: "proyecto-5-coworking-developer.appspot.com",
     messagingSenderId: "632752209221"
+  });
+}
+
+window.visitorRegister = (userName, userEmail, userHost) => {
+  let db = firebase.firestore();
+  const date = firebase.firestore.FieldValue.serverTimestamp();
+  db.collection('visitors').add({
+    userName: userName,
+    userEmail: userEmail,
+    userHost: userHost,
+    date: date, 
+    status: 0
+  })
+    .then(result => {
+      swal({
+        confirmButtonText: 'Aceptar',
+        type: 'success',
+        title: 'Visita REgistrada',
+        text: 'Debe esperar a la confirmación'
+      })
+    })
+    .catch(error => {
+      console.log('Tiene que registrarse primero', error);
     });
 }
-window.visitorRegister = (userName, userEmail, userHost) => {
-let db = firebase.firestore();
-const date = firebase.firestore.FieldValue.serverTimestamp();
-db.collection('visitors').add({ 
-  userName: userName,
-  userEmail: userEmail,
-  userHost: userHost,
-  date: date
-})
-.then(result => {
-  swal({
-    confirmButtonText: 'Aceptar',
-    type: 'success',
-    title:'Visita REgistrada',
-    text:'Debe esperar a la confirmación'
-  })
-})
 
-.catch(error => {
-  console.log('Tiene que registrarse primero', error);
-}); 
-}
 window.getHostList = () => {
-let db = firebase.firestore(); 
-let hostList= '';
-db.collection('Co-Workings').orderBy('Agencia', 'asc').get()
-.then(result => {
-  result.forEach(host => {
-hostList += `
-<option value = "${host.data().Agencia}">${host.data().Agencia}</option>`
-  }); 
-  document.getElementById('user-hots').innerHTML += hostList ;
-})
+  let db = firebase.firestore();
+  let hostList = '';
+  db.collection('Co-Workings').orderBy('Agencia', 'asc').get()
+    .then(result => {
+      result.forEach(host => {
+        hostList += `<option value = "${host.data().Agencia}">${host.data().Agencia}</option>`;
+      });
+      document.getElementById('user-hots').innerHTML += hostList;
+    })
+}
+
+window.drawStatusBadge = (status) =>{
+  let statusElements = '';
+  if(status === 1){
+    statusElements = '<span class="badge badge-success"><i class="fas fa-check-circle"></i> Aprovado</span>';
+  }else if(status === 0) {
+    statusElements = '<span class="badge badge-primary"><i class="fas fa-clock"></i> En espera</span>';
+  }else{
+    statusElements = '<span class="badge badge-danger"><i class="fas fa-times-circle"></i> Rechazado</span>';
+  }
+  return statusElements;
+}
+
+window.showUserCard = (userID) =>{
+  let db = firebase.firestore();
+  db.collection('visitors').doc(userID).get()
+  .then(result =>{
+    swal({
+      title: `<div class="small-text border-card"><p>Nombre: ${result.data().userName}</p><p>Correo: ${result.data().userEmail}</p><p>Host: ${result.data().userHost}</p><p>Fecha: ${result.data().date}</p></div>`
+    })
+  })
+}
+
+window.drawListOfVisitors = () =>{
+  let db = firebase.firestore();
+  let tableContent = '';
+  let i = 1;
+  db.collection('visitors').get()
+  .then(result =>{
+    result.forEach(visitor =>{
+      const status = drawStatusBadge(visitor.data().status);
+      tableContent += `<tr>
+        <th scope="row">${i++}</th>
+        <td>${visitor.data().userName}</td>
+        <td>${visitor.data().userEmail}</td>
+        <td>${visitor.data().userHost}</td>
+        <td>${visitor.data().date}</td>
+        <td>${status}</td>
+        <td><button class="no-btn"><span class="badge badge-warning" onclick="showUserCard('${visitor.id}')">Imprimir gafete</span></button></td>
+      </tr>`
+    });
+    document.getElementById('table-content').innerHTML = tableContent;
+  })
+  .catch(error =>{
+    console.log('Error', error);
+  })
+
 }
--- src/js/register.js
+++ src/js/register.js
@@ -1,12 +1,11 @@
 initializeFirebase();
 getHostList();
-document.getElementById('register-user').addEventListener('click', event =>{
+
+document.getElementById('register-user').addEventListener('click', event => {
     event.preventDefault();
     const userName = document.getElementById('user-name').value;
     const userEmail = document.getElementById('user-email').value;
     const userHost = document.getElementById('user-hots').value;
-     
+
     visitorRegister(userName, userEmail, userHost);
-
-
 })
